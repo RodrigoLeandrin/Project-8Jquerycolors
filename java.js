f9b90343cfa0a7b03c36a11fca5b3f1ce972f489
Rodrigo Vinicius Leandrin <108472050+RodrigoLeandrin@users.noreply.github.com>
2023-11-26 23:30:27 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,14 @@
+alert ("teste");
+
+function colorblue() {
+    $(".body").css("background-color","blue");
+}
+
+function colorred() {
+    $(".body").css("background-color","red");
+}
+
+
+function colorgreen() {
+    $(".body").css("background-color","yellowgreen");
+}
